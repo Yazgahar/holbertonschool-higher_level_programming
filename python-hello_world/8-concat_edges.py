bc8eb1cf119b20e0cba4098d15ec1963f6c1875f
yazgahar <battianas@hotmail.com>
2024-01-23 17:13:52 +0100

    8-concat_edges.py

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+       language that combines remarkable power with very clear syntax"
+NewStr = str.replace(f"{str}", "object-oriented programming with Python")
+print(NewStr)
