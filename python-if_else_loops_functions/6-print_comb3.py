9dc43626d2921ad8dd87e53bd5c0507b342ebc59
yazgahar <battianas@hotmail.com>
2024-01-24 17:01:40 +0100

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for a in range(0, 9):
     for b in range(a + 1, 10):
-        print(f"{a}{b}", end=", " if a != 8 or b != 9 else "\n")
+        if (a != 8 or b != 9):
+            print(f"{a}{b}", end=", ")
+        else:
+            print(f"{a}{b}",)
