817cd6e1905c0083bb6a484196e376247b6ab340
yazgahar <battianas@hotmail.com>
2024-01-25 10:47:38 +0100

    5-variable_load.py

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+if __name__ == "__main__":  # makes sure the code is not executed when imported
+    import variable_load_5  # imports variable_load_5 module
+    print(variable_load_5.a)  # prints the value of variable a
