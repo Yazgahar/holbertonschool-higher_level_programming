5689966c5cd622503cf2f2b5d3d05b5d9494c0cf
yazgahar <battianas@hotmail.com>
2024-02-06 15:54:19 +0100

    condition order changed

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,8 +10,8 @@ class Square:
         size (int): The size of the square
     """
     def __init__(self, size=0):
-        self.__size = size
         if size < 0:
             raise ValueError("size must be >= 0")
         if not isinstance:
             raise TypeError("size must be an integer")
+        self.__size = size
