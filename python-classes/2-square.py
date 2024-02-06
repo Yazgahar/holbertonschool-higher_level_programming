9040d1b144339af2b60ec765464bdec101d17ae0
yazgahar <battianas@hotmail.com>
2024-02-06 15:33:44 +0100

    description added

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Define a square"""
+
+
 class Square:
+    """
+    Initialize a new Square instance
+
+    Parameters:
+        size (int): The size of the square
+    """
     def __init__(self, size=0):
         try:
             self.__size = size
