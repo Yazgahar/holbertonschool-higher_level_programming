fbc73139f74c1a3af12d13371a3fcffd51ab71b8
yazgahar <battianas@hotmail.com>
2024-02-06 16:21:20 +0100

    description order changed

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Define a square"""
+"""Define a square class"""
 
 
 class Square:
     """
-    Initialize a new Square instance
-
-    Parameters:
-        size (int): The size of the square
+    Define a square
     """
     def __init__(self, size=0):
+        """
+        Initialize a new Square instance
+
+        Parameters
+            size (int): The size of the square
+         """
         if size < 0:
             raise ValueError("size must be >= 0")
         if not isinstance(size, int):
