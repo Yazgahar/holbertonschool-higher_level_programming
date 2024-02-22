1a28c7c0ec03b00a2d647126fdc3c6a490d05c76
yazgahar <battianas@hotmail.com>
2024-02-22 17:37:10 +0100

    pycodestyle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Module for the base class"""
+
 
 class Base:
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is None:
             Base.__nb_objects += 1
